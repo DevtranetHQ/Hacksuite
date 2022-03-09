017a9243b0f632ea367bd379b3a0d00d0f5e7f1e
Ellie <ytaghavi@yahoo.com>
2022-03-08 23:55:51 -0800

    add breakpoint for smaller screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
     content: ["./pages/**/*.js", "./components/**/*.js"],
     theme: {
+        screens: {
+            'xs': '428px',
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+          },
         extend: {
             colors: {
                 "deep-sky-blue": "#03A9F4",
