6c4aacfa3ec6f5531a0d851d922168ad0204e1f5
Muhammad Talha <sulemanshah432@gmail.com>
2022-03-17 02:26:00 +0530

    password eye in login page

--- pages/login.js
+++ pages/login.js
@@ -1,122 +1,127 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+import { Icon } from "@iconify/react";
 import DarkModeToggle from "../components/DarkModeToggle";
 import Logo from "../components/Logo";
 import authImage from "../public/assets/auth/auth-background.svg";
 import discordImage from "../public/assets/discord.svg";
 
 export default function Login({ darkMode, toggleDarkMode }) {
+    const [revealPassword, setRevealPassword] = useState(false);
+
+    const toggleReveal = () => {
+        setRevealPassword(!revealPassword);
+        var id = document.getElementById("password");
+        id.type = id.type === "password" ? "text" : "password";
+    };
     return (
-        <div className="dark:bg-[#202020] dark:text-white flex flex-col min-h-screen">
-            <div className="flex items-center justify-between px-12 py-5">
-                <Logo darkMode={darkMode} />
-                <DarkModeToggle
-                    className="w-[34px] h-[31px]"
-                    darkClassName="w-[25px] h-[35px]"
-                    darkMode={darkMode}
-                    toggleDarkMode={toggleDarkMode}
-                />
-            </div>
-            <div className="flex grow shrink basis-[auto] grid grid-cols-2">
-                <div className="flex items-end relative">
-                    <Image src={authImage} />
+        <div className="dark:bg-[#202020] dark:text-white relative">
+            <div className="flex items-center justify-between pl-8 pr-12">
+                <Logo darkMode={darkMode} className="w-[120px] pt-5" />
+                <div className="pt-2">
+                    <DarkModeToggle
+                        className="w-[34px] h-[31px]"
+                        darkClassName="w-[25px] h-[35px]"
+                        darkMode={darkMode}
+                        toggleDarkMode={toggleDarkMode}
+                    />
                 </div>
-                <div className="md:p-14">
-                    <h1 className="eyebrow text-black text-center">Log In</h1>
-                    <form>
-                        <div>
-                            <label className="form-label" htmlFor="email">
-                                Email Address
-                            </label>
-                            <input
-                                autoComplete="off"
-                                className="form-input"
-                                name="email"
-                                id="email"
-                                type="email"
-                                maxLength="80"
-                                placeholder="Email Address"
-                                required
-                            />
-                        </div>
-                        <div>
-                            <div className="flex items-center justify-between">
-                                <label
-                                    className="form-label !mb-0"
-                                    htmlFor="password">
-                                    Password
-                                </label>
+            </div>
+            <div className="flex flex-col">
+                <div className="flex">
+                    <div className="w-1/2 hidden xs:block p-0 lg:pl-4 xl:pl-20 2xl:pl-0 mx-auto 2xl:mx-0">
+                        <Image src={authImage} layout="responsive" alt="Dash" />
+                    </div>
+                    <div className="w-full xs:w-1/2 sm:w-2/5 lg:w-1/3 2xl:w-1/4 sm:mx-12 lg:mx-20 xl:ml-44 xl:mr-40 2xl:mx-auto 2xl:mt-20">
+                        <h1 className="text-30px mb-3 text-center font-black mb-1">
+                            LOG IN
+                        </h1>
+                        <form className="lg:pt-12 lg:pb-6 lg:px-12 rounded-3xl">
+                            <div>
+                                <div>
+                                    <label
+                                        className="text-12px md:text-18px font-bold"
+                                        htmlFor="email">
+                                        Email Address
+                                    </label>
+                                    <input
+                                        className="form-input text-12px md:text-16px rounded-lg py-1"
+                                        name="email"
+                                        id="email"
+                                        type="email"
+                                        maxLength="80"
+                                        placeholder="Email Address"
+                                        required
+                                    />
+                                </div>
+                                <div>
+                                    <div className="flex justify-between">
+                                        <label
+                                            className="text-12px md:text-18px font-bold"
+                                            htmlFor="password">
+                                            Password
+                                        </label>
+                                        <a
+                                            href="#"
+                                            className="text-gray-500 text-8px md:text-12px font-bold pt-2">
+                                            Forgot Password?
+                                        </a>
+                                    </div>
+                                    <input
+                                        className="form-input text-12px md:text-16px rounded-lg py-1"
+                                        name="password"
+                                        id="password"
+                                        type="password"
+                                        minLength="6"
+                                        maxLength="100"
+                                        placeholder="Password"
+                                        required
+                                    />
+                                    <Icon
+                                        className="-mt-11 mr-4 float-right inline text-gray-500"
+                                        onClick={toggleReveal}
+                                        width="1.3em"
+                                        height="1.3em"
+                                        icon={
+                                            revealPassword
+                                                ? "ant-design:eye-invisible-outlined"
+                                                : "ant-design:eye-outlined"
+                                        }
+                                    />
+                                </div>
+                                <button
+                                    className="w-1/2 button-small button-deep-sky-blue mx-auto text-12px md:text-16px py-1 rounded-sm mt-8"
+                                    type="submit">
+                                    Log in
+                                </button>
+                                <div className="flex justify-between caption -mx-10 lg:-mx-12 my-8">
+                                    <div className="w-1/4 h-4 border-gray-400 border-b-4"></div>
+                                    <div className="text-12px md:text-18px">
+                                        Or log in with
+                                    </div>
+                                    <div className="w-1/4 h-4 border-gray-400 border-b-4"></div>
+                                </div>
                                 <a
                                     href="#"
-                                    className="text-gray-500 dark:text-white text-8px md:text-12px font-bold">
-                                    Forgot Password?
+                                    className="button-small button-deep-sky-blue rounded-sm mx-auto text-12px lg:text-16px pl-8 sm:pr-0 lg:pr-auto">
+                                    The Dynamics Discord
+                                    <Image src={discordImage} height={24} />
                                 </a>
                             </div>
-                            <div className="form-input flex">
-                                <input
-                                    autoComplete="off"
-                                    className="focus:outline-none"
-                                    name="password"
-                                    id="password"
-                                    type="password"
-                                    minLength="6"
-                                    maxLength="100"
-                                    placeholder="Password"
-                                    required
-                                />
-                            </div>
-                        </div>
-                        <button
-                            className="button-small button-deep-sky-blue mx-auto"
-                            type="submit">
-                            Log in
-                        </button>
-                        <div className="flex justify-between caption my-5 -mx-10">
-                            <div className="w-1/4 h-4 border-gray-400 border-b-4"></div>
-                            <div className="dark:text-white">
-                                Or log in with
-                            </div>
-                            <div className="w-1/4 h-4 border-gray-400 border-b-4"></div>
-                        </div>
-                        <a
-                            href="#"
-                            className="button-small button-deep-sky-blue flex gap-2">
-                            <span>The Dynamics Discord</span>
-                            <svg
-                                width="20"
-                                height="20"
-                                viewBox="0 0 71 55"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <g clipPath="url(#clip0)">
-                                    <path
-                                        d="M60.1045 4.8978C55.5792 2.8214 50.7265 1.2916 45.6527 0.41542C45.5603 0.39851 45.468 0.440769 45.4204 0.525289C44.7963 1.6353 44.105 3.0834 43.6209 4.2216C38.1637 3.4046 32.7345 3.4046 27.3892 4.2216C26.905 3.0581 26.1886 1.6353 25.5617 0.525289C25.5141 0.443589 25.4218 0.40133 25.3294 0.41542C20.2584 1.2888 15.4057 2.8186 10.8776 4.8978C10.8384 4.9147 10.8048 4.9429 10.7825 4.9795C1.57795 18.7309 -0.943561 32.1443 0.293408 45.3914C0.299005 45.4562 0.335386 45.5182 0.385761 45.5576C6.45866 50.0174 12.3413 52.7249 18.1147 54.5195C18.2071 54.5477 18.305 54.5139 18.3638 54.4378C19.7295 52.5728 20.9469 50.6063 21.9907 48.5383C22.0523 48.4172 21.9935 48.2735 21.8676 48.2256C19.9366 47.4931 18.0979 46.6 16.3292 45.5858C16.1893 45.5041 16.1781 45.304 16.3068 45.2082C16.679 44.9293 17.0513 44.6391 17.4067 44.3461C17.471 44.2926 17.5606 44.2813 17.6362 44.3151C29.2558 49.6202 41.8354 49.6202 53.3179 44.3151C53.3935 44.2785 53.4831 44.2898 53.5502 44.3433C53.9057 44.6363 54.2779 44.9293 54.6529 45.2082C54.7816 45.304 54.7732 45.5041 54.6333 45.5858C52.8646 46.6197 51.0259 47.4931 49.0921 48.2228C48.9662 48.2707 48.9102 48.4172 48.9718 48.5383C50.038 50.6034 51.2554 52.5699 52.5959 54.435C52.6519 54.5139 52.7526 54.5477 52.845 54.5195C58.6464 52.7249 64.529 50.0174 70.6019 45.5576C70.6551 45.5182 70.6887 45.459 70.6943 45.3942C72.1747 30.0791 68.2147 16.7757 60.1968 4.9823C60.1772 4.9429 60.1437 4.9147 60.1045 4.8978ZM23.7259 37.3253C20.2276 37.3253 17.3451 34.1136 17.3451 30.1693C17.3451 26.225 20.1717 23.0133 23.7259 23.0133C27.308 23.0133 30.1626 26.2532 30.1066 30.1693C30.1066 34.1136 27.28 37.3253 23.7259 37.3253ZM47.3178 37.3253C43.8196 37.3253 40.9371 34.1136 40.9371 30.1693C40.9371 26.225 43.7636 23.0133 47.3178 23.0133C50.9 23.0133 53.7545 26.2532 53.6986 30.1693C53.6986 34.1136 50.9 37.3253 47.3178 37.3253Z"
-                                        fill="#ffffff"
-                                    />
-                                </g>
-                                <defs>
-                                    <clipPath id="clip0">
-                                        <rect
-                                            width="71"
-                                            height="55"
-                                            fill="white"
-                                        />
-                                    </clipPath>
-                                </defs>
-                            </svg>
+                        </form>
+                    </div>
+                </div>
+
+                <footer className="bg-deep-sky-blue lead text-white py-3">
+                    <div className="flex items-center justify-center">
+                        Don&#x27;t have an account?&nbsp;
+                        <a className="underline text-white" href="#">
+                            Become a member!
                         </a>
-                    </form>
-                </div>
+                    </div>
+                </footer>
             </div>
-            <footer className="block bg-deep-sky-blue py-2 lead text-white w-full">
-                <div className="flex items-center justify-center">
-                    Don't have an account?&nbsp;
-                    <Link href="/signup">
-                        <a className="underline text-white">Become a member!</a>
-                    </Link>
-                </div>
-            </footer>
         </div>
     );
 }
