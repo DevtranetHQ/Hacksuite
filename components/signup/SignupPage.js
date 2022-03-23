ce7f638bae9b21cdf5a68421a0cf81172ba6642c
Mohammed Talha <sulemanshah432@gmail.com>
2022-03-23 13:40:40 +0530

    signup - test reCaptcha

--- components/signup/SignupPage.js
+++ components/signup/SignupPage.js
@@ -137,7 +137,7 @@ export default function SignupPage({ handleSubmission, isLoading, method }) {
                         <ReCAPTCHA
                             className="w-fit mx-auto"
                             sitekey="6LexReUeAAAAAF5a0KmF1tz26MWEFUwnhQ7crZAL"
-                            size="normal"
+                            size="compact"
                             theme={darkMode? "dark": "light"}
                         />
                         <LoadingButton
