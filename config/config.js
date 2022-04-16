78ef7023f1c9c0bdfada94ce559c240837c92b94
Dev Agrawal <devagrawal09@gmail.com>
2022-04-15 21:38:59 -0400

    default url is public api url

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://api.thedynamics.tech";
 const config = {
     API_URL: API_URL,
     axios: axios.create({ baseURL: API_URL })
