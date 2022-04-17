bdf092180c952b9a35ef0cb579714d807964559f
Dev Agrawal <devagrawal09@gmail.com>
2022-04-17 14:53:19 -0400

    added cors to integration handlers

--- server/middlewares/pre-route.middleware.js
+++ server/middlewares/pre-route.middleware.js
@@ -1,35 +1,13 @@
 import cors from "cors";
-import path from "path";
-import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-import express from "express";
+import { withMiddleware } from "./../utils/withMiddleware";
 
-const preRoute = app => {
-    // Set Env File
-    dotenv.config({
-        path: path.resolve(__dirname, "..", "..", ".env")
-    });
-
-    // enable CORS
-    app.use(cors());
-
-    // Secure the app by setting various HTTP headers off.
-    app.use(helmet());
-
-    // Logger
-    app.use(morgan("common"));
-
-    // Tell express to recognize the incoming Request Object as a JSON Object
-    app.use(express.json());
-
-    // Express body parser
-    app.use(express.urlencoded({ extended: true }));
-
-    // Serve Uploads
-    app.use("/uploads", express.static("../../uploads"));
-
-    return app;
+export const withPreRouteHandlers = handler => {
+    // use pre-route express middleware
+    return withMiddleware(helmet(), withMiddleware(morgan("common"), handler));
 };
 
-export default preRoute;
+export const withCors = handler => {
+    return withMiddleware(cors(), handler);
+};
