22282abaa083dc4850c3e5c6dac2c9bf1fe90251
Dev Agrawal <devagrawal09@gmail.com>
2022-04-20 21:21:43 -0400

    fixed broken import

--- pages/api/integrations/discord/resend-verification-email.js
+++ pages/api/integrations/discord/resend-verification-email.js
@@ -1,5 +1,5 @@
 import { IntegrationCtrl } from "../../../../server/controllers/integration.controller";
-import { withCors } from "../../../../server/middlewares/corrs.middleware";
+import { withCors } from "../../../../server/middlewares/cors.middleware";
 import { withGlobalMiddleware } from "./../../../../server/middlewares/global.middleware";
 
 function handler(req, res) {
