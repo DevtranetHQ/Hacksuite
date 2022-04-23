60f7c342fdedb20e818720159e4998184cef88a3
oyerohabib <oyerohabib@yahoo.com>
2022-04-23 08:38:46 +0100

    added go back home button in 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,21 +1,20 @@
 import Image from "next/image";
+import Link from "next/link"
 import errorImage from "../public/assets/404.svg";
-import HomeImage from "../public/assets/home.jpg";
+import HomeImage from "../public/assets/home.png";
 
 export default function Error() {
     return (
         <>
         <Image layout="fill" objectFit="contain" src={errorImage} alt="404" />
-        {/* <div className="button"> */}
-            {/* {HomeImage} */}
-            {/* go back home */}
-        {/* </div> */}
-        {/* <div className="button-small button-fruit-salad text-12px xs:text-24px w-fit mx-auto mt-4 xs:mt-5 rounded xs:rounded-md px-3 xs:px-8 cursor-pointer h-6 xs:h-12">
-            <span className="mt-0.5 xs:mt-1">
-                <InboxIcon />
-            </span>
-            &ensp;resend email
-        </div> */}
+        <div className="absolute mt-6 top-[82%] md:top-[90%] lg:top-[91%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+            <Link href="/login">
+            <div className="bg-fruit-salad p-2 border-0 rounded-md cursor-pointer flex justify-center items-center">
+                <Image src={HomeImage} layout="fixed" className="" alt="home image" height={16.5} width={19.5} />
+                <span className="text-white capitalize">&nbsp;&nbsp;go back home</span>
+            </div>
+            </Link>
+        </div>
         </>
     );
 }
