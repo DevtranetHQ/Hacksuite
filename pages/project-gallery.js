ef0d9772deb8d8aacbbae4011d4e7686f4b030b9
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-04-26 14:33:33 +0100

    Added the profile cards to the project gallery

--- pages/project-gallery.js
+++ pages/project-gallery.js
@@ -3,10 +3,17 @@ import DarkModeToggle from "../components/DarkModeToggle";
 import GithubIcon from "../components/icons/Github";
 import Link from "next/link";
 import ArrowIcon from "../components/icons/Arrow";
+import ProfileProjectCard from "../components/project/ProfileProjectCard";
+
+export const bubbleTrimmer = (bubbles, start = 0, end = 0) => {
+    // prettier-ignore
+    return bubbles && bubbles.length > end
+              ? bubbles.slice(start, end)
+              : bubbles
+  };
 
 
-
-export default function ProjectGallery(props) {
+export default function ProjectGallery({user, project}) {
     return (
         <div className="dark:bg-[#202020] dark:text-white">
             <nav className="flex items-center justify-between pl-8 pr-12">
@@ -39,6 +46,112 @@ export default function ProjectGallery(props) {
             </a>
             </div>
       </header>
+        <div className="grid gap-10 xs:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 content-center justify-center mt-[90px]">
+          {user &&
+            user.projects.map((project, index) => {
+              return (
+                <ProfileProjectCard
+                  key={index}
+                  bubbles={bubbleTrimmer(project.bubbles, 0, 3)}
+                  date="ferbrary 28, 2020"
+                  title="Web Scrapper"
+                  likes={93}
+                  image={project.image}
+                  comments={27}
+                  tags={bubbleTrimmer(project.tags, 0, 4)}
+                  desc={project.desc}
+                />
+              );
+            })}
+        </div>
+
         </div>
     )
 }
+
+export async function getServerSideProps(context) {
+    // TODO: Call API for User profile data
+    return {
+      props: {
+        loggedIn: false,
+        user: {
+          name: "Zach Latter",
+          no_of_followers: 10,
+          followers: [
+            { image: "/assets/TEST/img-1.jpg" },
+            { image: "/assets/TEST/img-2.jpg" },
+            { image: "/assets/TEST/img-3.jpg" },
+            { image: "/assets/TEST/img-4.jpg" },
+            { image: "/assets/TEST/img-5.jpg" },
+            { image: "/assets/TEST/img-6.jpg" },
+            { image: "/assets/TEST/img-7.jpg" },
+            { image: "/assets/TEST/img-8.jpg" },
+            { image: "/assets/TEST/img-9.jpg" },
+            { image: "/assets/TEST/img-10.jpg" }
+          ],
+          projects: [
+            {
+              bubbles: [1, 2, 3, 4, 5, 6],
+              date: "ferbrary 28, 2020",
+              title: "Web Scrapper",
+              desc: "A chrome extension that gathers vital information a the tap of a button, easy as ABC",
+              image: "/assets/TEST/user_projects/img-1.png",
+              comments: 22222,
+              likes: 33333333,
+              tags: ["NextJs", "Figma"]
+            },
+            {
+              bubbles: [1, 2, 3],
+              date: "ferbrary 28, 2020",
+              title: "Tesla",
+              desc: "Launched the first prototype of the world’s firts self-driving vehicle. Best part: 100% AI",
+              image: "/assets/TEST/user_projects/img-2.png",
+              comments: 22222,
+              likes: 33333333,
+              tags: ["React", "Vue", "Express", "Laravel"]
+            },
+            {
+              bubbles: [2],
+              date: "ferbrary 28, 2020",
+              title: "Codetivate",
+              desc: "The world’s largest diversity-focused hackhon web application built for this fall 2022",
+              image: "/assets/TEST/user_projects/img-3.png",
+              comments: 22222,
+              likes: 33333333,
+              tags: ["PHP", "Golang", "Adobe XD"]
+            },
+            {
+              bubbles: [],
+              date: "ferbrary 28, 2020",
+              title: "Command tech",
+              desc: "Advancing the partcipation of non-binary and female students in STEM worlwide ",
+              image: "/assets/TEST/user_projects/img-4.png",
+              comments: 22222,
+              likes: 33333333,
+              tags: ["Python"]
+            },
+            {
+              bubbles: [1, 2, 3, 4, 5, 6],
+              date: "ferbrary 28, 2020",
+              title: "Frelapay",
+              desc: "Get payments from all your freelance work converted into the highest selling cryptos",
+              image: "/assets/TEST/user_projects/img-5.png",
+              comments: 22222,
+              likes: 33333333,
+              tags: ["HTML", "CSS", "JSON"]
+            },
+            {
+              bubbles: [1, 2],
+              date: "ferbrary 28, 2020",
+              title: "Microsoft",
+              desc: "Coded Windows 7 a new OS from my dorm room, probably gonna dropout soon :(",
+              image: "/assets/TEST/user_projects/img-6.png",
+              comments: 22222,
+              likes: 33333333,
+              tags: ["Git", "Flask", "Django"]
+            }
+          ]
+        }
+      }
+    };
+  }
