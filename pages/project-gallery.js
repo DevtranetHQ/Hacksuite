fc7740cde9777bd797ba7d79aa70afc788c85a39
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-04-28 11:24:14 +0100

    Made the header section and more projects button responsive

--- pages/project-gallery.js
+++ pages/project-gallery.js
@@ -18,7 +18,7 @@ export default function ProjectGallery({user, project}) {
         <div className="dark:bg-[#202020] dark:text-white">
             <nav className="flex items-center justify-between pl-[10px] lg:pl-8 pr-[12px] lg:pr-12">
             <Logo className="w-[80px] md:w-[120px] py-5" />
-            <div className="flex gap-x-[0px] md:gap-x-2 items-center">
+            <div className="flex gap-x-[0px] md:gap-x-2 lg:gap-x-6 items-center">
             <DarkModeToggle
                 className="mx-0 md:w-[40px] md:h-[40px]"
                 darkClassName="mx-0 w-[25px] md:w-[40px] h-[25px] md:h-[42px]"
@@ -27,7 +27,7 @@ export default function ProjectGallery({user, project}) {
                 <GithubIcon />
             </a>
             <Link href="/profile/1">
-                <button className="px-[10px] py-[6px] bg-[#03a9f4] text-white rounded-[3px] text-[12px] md:button-medium button-deep-sky-blue inline-flex gap-x-1 md:gap-x-3 items-center mx-2 my-0 md:my-0">
+                <button className="px-[10px] py-[6px] bg-[#03a9f4] text-white rounded-[3px] text-[12px] md:text-[23px] lg:text-[30px] md:button-medium lg:button-big button-deep-sky-blue inline-flex gap-x-1 md:gap-x-3 items-center mx-2 my-0 md:my-0">
                     Go back home
                 <div className="scale-75 md:scale-100">
                     <ArrowIcon />
@@ -37,13 +37,13 @@ export default function ProjectGallery({user, project}) {
             </div>
         </nav>
         <header className="bg-[#F8FBFF] container-gray-dark border-b-4 dark:border-gray-dark p-14 rounded-b-2xl text-center">
-            <h1 className="text-[30px] font-bold lg:title text-deep-sky-blue">The Dynamics Projects</h1>
-            <h2 className="text-[16px] lg:lead mb-2 w-full mt-[16px] md:mt-[36px] md:mb-[28px]">A gallery of all the awesome things the makers at The Dynamics are building and launching everyday.</h2>
-            <div className="inline-flex my-2 gap-x-4 mt-[20px]">
-            <button className="button-small py-[15px] text-[16px] px-[15px] lg:button-medium button-fruit-salad inline-flex gap-x-2">
+            <h1 className="text-[30px] font-bold lg:title md:text-[65px] lg:text-[90px] text-deep-sky-blue">The Dynamics Projects</h1>
+            <h2 className="text-[16px] lg:lead mb-2 w-full mt-[16px] md:mt-[36px] md:mb-[28px] lg:text-[21px] xl:text-[26px] 2xl:text-[30px]">A gallery of all the awesome things the makers at The Dynamics are building and launching everyday.</h2>
+            <div className="inline-flex my-2 gap-x-4  lg:gap-x-[36px] mt-[20px] lg:mt-[30px]">
+            <button className="text-white font-bold rounded-md bg-[#4CB050] py-[15px] text-[16px] px-[15px] lg:px-[70px] lg:py-[10px] lg:text-[30px]  inline-flex gap-x-2">
                 All Projects
             </button>
-            <a className="button-small py-[15px] text-[16px] px-[15px] lg:button-medium button-deep-sky-blue inline-flex gap-x-2" href="#comments">
+            <a className="text-white font-bold rounded-md bg-[#03A9F4] py-[15px] text-[16px] px-[15px] lg:px-[70px] lg:py-[10px] lg:text-[30px]  inline-flex gap-x-2" href="#comments">
                 Add your project
             </a>
             </div>
@@ -68,13 +68,15 @@ export default function ProjectGallery({user, project}) {
             })}
         </div>
         <div className="w-full flex justify-center items-center mt-[70px] lg:mt-[167px] mb-[45px] lg:mb-[90px]">
-            <button className="button-medium lg:button-big button-deep-sky-blue inline-flex gap-x-3">
-                View more Projects 
-                <ArrowIcon />
+            <button className="text-white text-[16px] lg:text-[48px] py-[13px] lg:py-[48px] px-[17px] lg:px-[44px] font-bold rounded-md bg-[#03a9f4] lg:button-big button-deep-sky-blue inline-flex items-center gap-x-3 lg:gap-x-[48px]">
+                <span>View more Projects</span>
+                <div className="lg:scale-[2] relative lg:top-1">
+                  <ArrowIcon />
+                </div>
             </button>
         </div>
         <footer className="bg-[#F4F4F4] dark:bg-[#444444] py-[32px]">
-            <p className="text-[16px] lg:text-[32px] px-[40px] text-center">You’ve reached the end, why not <a href="">become a member</a> and show us all the cool things you’ve made?</p>
+            <p className="text-[16px] md:text-[24px] lg:text-[32px] px-[40px] text-center">You’ve reached the end, why not <a href="">become a member</a> and show us all the cool things you’ve made?</p>
         </footer>
         </div>
     )
--- server/database/index.js
+++ server/database/index.js
@@ -6,19 +6,19 @@ const { MONGODB_URI } = config;
 
 const log = debug(`app:database`);
 
-mongoose.connect(
-  MONGODB_URI,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  },
-  (err, data) => {
-    if (err) {
-      console.error("<::: Couldn't connect to database", err);
-    } else {
-      log(`:::> Connected to MongoDB database. ${MONGODB_URI}`);
-    }
-  }
-);
+// mongoose.connect(
+//   MONGODB_URI,
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   },
+//   (err, data) => {
+//     if (err) {
+//       console.error("<::: Couldn't connect to database", err);
+//     } else {
+//       log(`:::> Connected to MongoDB database. ${MONGODB_URI}`);
+//     }
+//   }
+// );
 
 export default mongoose;
