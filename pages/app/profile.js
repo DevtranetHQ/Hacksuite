e30622ccd8f2a8e7a39d1baa62df41cbb61c950b
Dev Agrawal <agrawadv@mail.uc.edu>
2022-04-29 19:53:07 -0400

    removed broken import

--- pages/app/profile.js
+++ pages/app/profile.js
@@ -4,12 +4,10 @@ import Logo from "../../components/Logo";
 import TelInput from "../../components/form/TelInput";
 import CountryInput from "../../components/form/CountryInput";
 import { withAuth } from "../../server/middlewares/auth.middleware";
-import { useProfile } from "./../../hooks/useProfile";
 import Fade from "react-reveal/Fade";
 
 export default function Profile({ user }) {
   const verifyEmail = "success";
-  const { completeProfile } = useProfile();
 
   const onSubmit = e => {
     e.preventDefault();
@@ -26,7 +24,7 @@ export default function Profile({ user }) {
       levelOfStudy: formData.get("levelOfStudy")
     };
 
-    completeProfile.execute(user.id, data);
+    console.log(data)
   };
 
   return (
