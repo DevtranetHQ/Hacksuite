811cda7b09a80f7848a658564ed4f22deeb3c61f
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-05-03 12:27:59 +0100

    Increased the size of text in the text section

--- components/project/ProjectGalleryProjectCard.js
+++ components/project/ProjectGalleryProjectCard.js
@@ -81,7 +81,7 @@ export default function ProjectGalleryProjectCard({ ...props }) {
       {/* ====== #TEXT SECTION */}
       <div className="w-full mt-[5px]">
         <h4 className=" text-[20.3px] font-semibold">{title}</h4>
-        <p className="text-[11.6px] mt-[10px]">{desc}</p>
+        <p className="text-[13.5px] mt-[10px]">{desc}</p>
       </div>
 
       {/* ====== #BOTTOM SECTION */}
