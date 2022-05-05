aaefeaec9ae447345698da8abf9dd405b96e51e3
Zobam <zobamokoli7@gmail.com>
2022-05-05 15:41:10 +0100

    Revert "Delete components/Scrapbook directory"

---
+++ components/Scrapbook/CardsLine2.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import CardsLine1a from '../../components/Scrapbook/CardsLine1a'
+import CardsLine1c from '../../components/Scrapbook/CardsLine1b'
+import CardsLine1d from '../../components/Scrapbook/CardsLine1c'
+import CardsLine1b from '../../components/Scrapbook/CardsLine1d'
+
+function CardsLine2() {
+  return (
+    <div className='cardsline2'>
+        <CardsLine1c/>
+        <CardsLine1d/>
+        <CardsLine1a/>
+        <CardsLine1d/>
+        <CardsLine1b/>
+        <CardsLine1c/>
+        <CardsLine1b/>
+    </div>
+  )
+}
+
+export default CardsLine2
