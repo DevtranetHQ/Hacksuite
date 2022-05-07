0581dd201551c8f76c76f01317da1b9aaaaff941
code-art4 <ojotriumph4@gmail.com>
2022-05-07 10:31:57 +0100

    redid the mobile responsiveness of the app index page

--- components/dash/DashNav.js
+++ components/dash/DashNav.js
@@ -4,14 +4,17 @@ import { useContext } from "react";
 import styles from "./DashNav.module.css";
 import DarkModeContext from "../DarkModeContext";
 import Logo from "../Logo";
+import TopNav from './TopNav';
 
-export default function DashNav({ admin }) {
+export default function DashNav({ admin, unread }) {
   const router = useRouter();
   const { pathname } = router;
   const { darkMode } = useContext(DarkModeContext);
   
   return (
-    <nav className="border-r-2 min-h-screen container-gray-dark dark:text-white dark:border-r-0 pr-10 pl-10 z-20  flex-col justify-center  fixed top-0 left-0 mx-auto">
+    <>
+    <TopNav unread={unread}/>
+    <nav className="border-r-2 min-h-screen container-gray-dark dark:text-white dark:border-r-0 pr-10 pl-10 z-20  flex-col justify-center  fixed top-0 left-0 mx-auto hidden md:block">
       <Logo className="mx-auto pt-5 w-[100px]" darkMode={darkMode} />
       <div className={styles.nav}>
         <Link href="/app">
@@ -224,5 +227,6 @@ export default function DashNav({ admin }) {
         </Link>
       </div>
     </nav>
+    </>
   );
 }
