303c467044cc7110098d1a6f6383f760346aa532
Zobam <zobamokoli7@gmail.com>
2022-05-10 21:19:39 +0100

    Create-new project

--- pages/app/create-project.js
+++ pages/app/create-project.js
@@ -12,57 +12,59 @@ import GitHubIcon from "../../components/icons/Github";
 import FigmaIcon from "../../components/icons/Figma";
 import AdobeIcon from "../../components/icons/Adobe";
 import NotificationsLink from "../../components/dash/NotificationsLink";
-import Select, { components } from 'react-select';
-import { Icon } from '@iconify/react';
+import Select, { components } from "react-select";
+import { Icon } from "@iconify/react";
 
 export default function CreateProject({ recaptchaSitekey, choices, unread }) {
-  // TODO: Work on adding colloborators
-  const handleSubmission = () => {
-
-  }
-
+  const handleSubmission = () => {};
+  
+  
   // Multiple Select Functions
+  // TODO: Work on adding colloborators
   const options = [
-    { value: 'Developer', label: 'Developer' },
-    { value: 'Founder', label: 'Founder' },
-    { value: 'Student', label: 'Student' },
-    { value: 'Designer', label: 'Designer' },
+    { value: "Developer", label: "Elon Musk", image: "/assets/TEST/user_projects/img-6.png" },
+    { value: "Founder", label: "Melinda Gates" , image: "/assets/TEST/user_projects/img-6.png" },
+    { value: "Student", label: "Mark Zuckerberg" , image: "/assets/TEST/user_projects/img-6.png" },
+    { value: "Designer", label: "Zach Latta" , image: "/assets/TEST/user_projects/img-6.png"}
   ];
   const options2 = [
-    { value: 'TypeScripts', label: 'Typescripts' },
-    { value: 'Python', label: 'Python' },
-    { value: 'React', label: 'React' },
-    { value: 'Robotics', label: 'Robotics' },
-    { value: 'Angular', label: 'Angular' },
+    { value: "TypeScripts", label: "Typescripts" },
+    { value: "Python", label: "Python" },
+    { value: "React", label: "React" },
+    { value: "Robotics", label: "Robotics" },
+    { value: "Angular", label: "Angular" }
   ];
   const styles = {
-    control: (provided) => ({
+    control: provided => ({
       ...provided,
       border: 0,
       outline: "none",
       boxShadow: "none"
     }),
+    menu: styles => ({ ...styles,                 
+      width: '500px',
+     }),  
     option: (provided, state) => ({
       ...provided,
-      "&:hover" : {
-        backgroundColor : "#03A9F4",
-        color: "white",
+      "&:hover": {
+        backgroundColor: "#03A9F4",
+        color: "white"
       },
-      padding : 3
+      "padding": 3,
     }),
     multiValueRemove: (styles, { data }) => ({
       ...styles,
-      color: data.color,
-      ':hover': {
-        backgroundColor:" #03A9F4",
-        color: 'white',
-      },
-    }),
-  }
+      "color": data.color,
+      ":hover": {
+        backgroundColor: " #03A9F4",
+        color: "white"
+      }
+    })
+  };
   const CaretDownIcon = () => {
     return <Icon icon="bxs:down-arrow" color="#8a8a8a" width={15} height={20} inline={true} />;
   };
-  
+
   const DropdownIndicator = props => {
     return (
       <components.DropdownIndicator {...props}>
@@ -80,7 +82,7 @@ export default function CreateProject({ recaptchaSitekey, choices, unread }) {
   };
 
   const [image, setImage] = useState({
-    desc: "No image uploaded.",
+    desc: "",
     image: null
   });
   const updateImage = e => {
@@ -172,21 +174,21 @@ export default function CreateProject({ recaptchaSitekey, choices, unread }) {
           <div className="text-right flex items-end justify-end  mt-3">
             <DarkModeToggle className="h-[30px]" darkClassName="h-[30px]" />
             <Link href="/app/notifications">
-            <NotificationsLink className="h-[25px]" unread={unread}/>
+              <NotificationsLink className="h-[25px]" unread={unread} />
             </Link>
           </div>
         </header>
-        <hr className="mb-5 border-t-[1.4px] border-solid border-[#C9C9C9]"/>
-        <section className="pt-7 px-7">
+        <hr className=" border-t-[1.4px] border-solid border-[#C9C9C9]" />
+        <section className="pt-5 px-7">
           <p className="text-center">
-            Here you can share all the cool things you’re making and launching with The Dynamics
-            Community, and beyond!
+            Here you can share all the cool things you’re making and launching with{" "}
+            <span className="font-bold">The Dynamics Community,</span> and beyond!
           </p>
           <form className="bg-transparent dark:bg-transparent pl-0" onSubmit={handleSubmission}>
             <section className="mb-5">
               <div>
                 <label className="subheadline" htmlFor="name">
-                  Project Name
+                  Project name
                   <span className="text-[#ff0000]">*</span>
                 </label>
                 <input
@@ -203,12 +205,12 @@ export default function CreateProject({ recaptchaSitekey, choices, unread }) {
             </section>
             <section className="mb-14">
               <label className="subheadline">
-                Project Picture
+                Project picture
                 <span className="text-[#ff0000]">*</span>
               </label>
-              <div className="border-orange-peel border-4 mx-14 my-4 p-14 rounded-md text-center">
+              <div className="border-orange-peel border-4 mx-14 my-4 p-28  text-center rounded-lg flex-col items-center justify-center">
                 <label
-                  className="cursor-pointer button-medium button-deep-sky-blue mx-auto w-fit"
+                  className="cursor-pointer button-small button-deep-sky-blue gap-x-2 mx-auto w-fit"
                   htmlFor="picture">
                   <span className="mr-1">
                     <UploadIcon width={20} height={20} />
@@ -225,7 +227,7 @@ export default function CreateProject({ recaptchaSitekey, choices, unread }) {
                   Description
                   <span className="text-[#ff0000]">*</span>
                 </label>
-                <TextEditor onUpdate={updateDescription} />
+                <TextEditor onUpdate={updateDescription} />          
                 <label className="font-bold italic text-[#A5A5A5]">
                   Tell us more about this project, a typical description talks about what it does,
                   how it works, how it was built, some challenges encountered during the process, or
@@ -233,21 +235,81 @@ export default function CreateProject({ recaptchaSitekey, choices, unread }) {
                 </label>
               </div>
             </section>
+            <section className="mb-5">
+              <div>
+                <h2 className="subheadline mb-5">Add Collaborators</h2>
+                <div className="relative rounded-md flex  items-center mb-5">
+                  <div className="absolute pl-3 z-10 top-0 inset-y-0  flex items-center pointer-events-none">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5 text-black flex items-center dark:text-black"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}>
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                      />
+                    </svg>
+                  </div>
+                  {/* <input
+                    type="search"
+                    placeholder="Search"
+                    className=" block w-full pl-10 form-input dark:bg-transparent  rounded-lg"
+                  /> */}
+                  <Select
+                  className="pl-7 form-input p-1 m-0 rounded-lg"
+                  styles={styles}
+                  components={{ DropdownIndicator : () => null, IndicatorSeparator:() => null }}
+                  isMulti
+                  options={options}
+                  formatOptionLabel={option => (
+                    <div className="flex items-center ">
+                      <img src={option.image} alt="image" className="w-7 h-7 rounded-full border-2 border-orange-peel object-cover " />
+                      <span className="ml-2">{option.label}</span>
+                    </div>
+                  )}
+                  isClearable={false}
+                />
+                  <div className="inline-flex absolute right-0 p-3 top-0 gap-x-2 items-center">
+                    <span className="dark:text-black">Or invite via link</span>
+                    <LinkIcon fill="#FF9700" width={39.17} height={19.58} />
+                    
+                  </div>
+                </div>
+                {/* <input
+                
+                  autoComplete="off"
+                  className="form-input mb-4"
+                  id="name"
+                  placeholder="Describe your project in one or two words"
+                  onChange={updateName}
+                  type="text"
+                  value={name}
+                /> */}
+                <label className="font-bold italic text-[#A5A5A5]">
+                  Tell us the team behind this amazing project, if it’s just you leave this section
+                  blank.
+                </label>
+              </div>
+            </section>
             <h2 className="mb-5 subheadline">
               Technology/tools used or project domain<span className="text-[#ff0000]">*</span>
             </h2>
-            
+
             <section className="mb-5">
               <div className="mb-4">
                 <Select
-                className="form-select p-1 m-0 rounded-lg"
+                  className="form-select p-1 m-0 rounded-lg"
                   styles={styles}
                   components={{ DropdownIndicator }}
                   isMulti
                   options={options2}
                 />
               </div>
-            
+
               {/* <div className="form-input flex flex-wrap gap-2 relative z-[2] min-h-[47px]">
                 {Array.from(technologies).map((value, key) => (
                   <button
@@ -284,14 +346,14 @@ export default function CreateProject({ recaptchaSitekey, choices, unread }) {
               Project development stage<span className="text-[#ff0000]">*</span>
             </h2>
             <section className="mb-5">
-              <select className="form-input form-select" name="stage">
+              <select className="form-input form-select mb-5" name="stage">
                 {choices.stages.map((value, key) => (
                   <option key={key} value={value}>
                     {value}
                   </option>
                 ))}
               </select>
-              <label className="font-bold italic text-[#A5A5A5]" htmlFor="choices">
+              <label className="font-bold italic text-[#A5A5A5] " htmlFor="choices">
                 Tell us about the current state of this project, e.g beta, feature update,
                 completed, bug fixes, etc.
               </label>
@@ -338,7 +400,7 @@ export default function CreateProject({ recaptchaSitekey, choices, unread }) {
                     />
                   </div>
                   <div className="flex items-center gap-x-5">
-                    <FigmaIcon width={22.67} height={34}/>
+                    <FigmaIcon width={22.67} height={34} />
                     <input
                       className="default-input bg-[#E6E6E6] border border-[#442929] focus:outline-none m-0 px-2 py-0.5 rounded text-15px "
                       placeholder="Figma..."
@@ -365,17 +427,21 @@ export default function CreateProject({ recaptchaSitekey, choices, unread }) {
               </div>
               <label className="font-bold italic text-[#A5A5A5]">Max of 3 please.</label>
             </section>
-            <section className="text-center">
+            <section className="text-center mt-5">
               <ReCAPTCHA
                 className="inline-block mb-3"
                 sitekey={recaptchaSitekey}
                 onChange={i => console.log(i)}
               />
-              <div className="flex items-center justify-center gap-x-16">
-                <button className="button-medium button-orange-peel" type="submit">
+              <div className="flex items-center justify-center gap-x-14 mt-5">
+                <button
+                  className="button-big   px-20 w-[230px] text-22px  button-orange-peel"
+                  type="submit">
                   Save for later
                 </button>
-                <button className="button-medium button-deep-sky-blue" type="submit">
+                <button
+                  className="button-big button-deep-sky-blue  px-20 w-[230px] text-22px "
+                  type="submit">
                   Publish now
                 </button>
               </div>
