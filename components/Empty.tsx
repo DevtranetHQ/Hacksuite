b382e95fdfda1f280066a4fafd715d591f55bbb8
Muhammad Talha <sulemanshah432@gmail.com>
2022-05-13 03:23:35 +0530

    notification page mobile view

--- components/Empty.tsx
+++ components/Empty.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function Empty() {
   return (
-    <div className="flex flex-col relative bg-transparent border-[10px] border-[#C4C4C4] rounded-lg items-center justify-center mx-auto md:h-[400px] md:w-[1200px] w-[370px] h-[271px]">
+    <div className="w-[300] h-[600] flex flex-col relative bg-transparent border-[10px] border-[#C4C4C4] rounded-lg items-center justify-center mx-auto md:h-[400px] md:w-[1200px] xs:w-[370px] xs:h-[271px]">
       <p className="text-[#A5A5A5] font-bold font-body md:text-[72px] text-[24px] mb-3 ">
         WOW, SUCH EMPTY!
       </p>
