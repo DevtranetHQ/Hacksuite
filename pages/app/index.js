9ef732ed528f640a2f139a7ebed78d773da68a06
Zobam <zobamokoli7@gmail.com>
2022-05-23 19:36:53 +0100

    Home page tweaks

--- pages/app/index.js
+++ pages/app/index.js
@@ -16,27 +16,30 @@ export default function Dash({ admin, name }) {
 
   return (
     <div className="xs:grid xs:grid-cols-12 dark:bg-[#202020]">
-      <div className="mxs:hidden col-span-1 mx-auto z-50">
+      <div className="mxs:hidden col-span-1 mx-auto z-50 relative">
         <DashNav admin={admin} />
       </div>
 
       <div className="mxs:flex mxs:flex-col mxs:justify-between mxs:px-0 mxs:pt-4 dark:bg-[#202020] dark:text-white col-span-11 p-10  mx-auto content-center min-w-full min-h-screen">
-        <div className="flex items-center pl-32 mb-10 fixed left-40 right-0  z-40 bg-white dark:bg-dark">
+        <div className="flex items-center pl-32 mb-10 fixed left-40 right-0  z-40 bg-white dark:bg-dark justify-center mx-auto top-0 pt-10">
           <div className="flex mx-auto gap-20 items-center font-bold text-[#7D7D7D] text-24px ">
-            <Link href="/scrapbook">
-              <p className="hover:text-orange-peel transition-all cursor-pointer">SCRAPBOOK</p>
-            </Link>
-            <Link href="/project-gallery">
-              <p className="hover:text-orange-peel transition-all cursor-pointer">PROJECTS</p>
-            </Link>
-            <Link href="/">
-              <p className="hover:text-orange-peel transition-all cursor-pointer">PEOPLE</p>
-            </Link>
-            <Link href="/">
-              <p className="hover:text-orange-peel transition-all cursor-pointer">JOBS</p>
-            </Link>
+            <p className=" transition-all cursor-pointer border-b-4 border-transparent hover:border-orange-peel hover:text-black hover:dark:text-white">
+              SCRAPBOOK
+            </p>
+
+            <p className=" transition-all cursor-pointer border-b-4 border-transparent hover:border-orange-peel hover:text-black hover:dark:text-white">
+              PROJECTS
+            </p>
+
+            <p className="transition-all cursor-pointer border-b-4 border-transparent hover:border-orange-peel hover:text-black hover:dark:text-white ">
+              PEOPLE
+            </p>
+
+            <p className=" transition-all cursor-pointer border-b-4 border-transparent hover:border-orange-peel hover:text-black hover:dark:text-white">
+              JOBS
+            </p>
           </div>
-          <form className="bg-transparent flex items-center ml-auto m-0 justify-center pr-2 dark:bg-transparent text-white dark:bg-white p-0">
+          <form className="bg-transparent flex items-center mx-auto justify-center pl-5 dark:bg-transparent text-white dark:bg-white p-0">
             <div className="relative rounded-md flex items-center p-0 border-[#03A9F4] border-[3px] md:w-full ml-auto">
               <div className="absolute md:pl-3 pl-1 z-10 top-0 inset-y-0  flex items-center pointer-events-none md:w-full w-2/4">
                 <svg
@@ -60,7 +63,9 @@ export default function Dash({ admin, name }) {
               />
             </div>
           </form>
-          <DashHeader />
+          <div className="mx-auto pr-5">
+            <DashHeader />
+          </div>
         </div>
 
         <div className="text-center mt-20">
@@ -68,10 +73,10 @@ export default function Dash({ admin, name }) {
             Hey there, <span className="text-fruit-salad">{name}.</span>
           </h1>
           <div className="mx-auto -mt-2 xs:-mt-8">
-            <Image src={placeholder} alt="" width={700} height={430} />
+            <Image src={placeholder} alt="" width={700} height={398} />
           </div>
           <div className="-mt-6 xs:-mt-14">
-            <h1 className="mxs:text-24px text-54px -rotate-6">Welcome to</h1>
+            <h1 className="mxs:text-24px text-48px -rotate-6">Welcome to</h1>
             <h1 className="mxs:text-26px text-54px font-semibold uppercase -mt-3">The Dynamics</h1>
           </div>
         </div>
