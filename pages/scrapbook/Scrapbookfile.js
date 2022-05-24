d4f3b31b117aae80a36d955151675b5767d56b65
Zobam <zobamokoli7@gmail.com>
2022-05-24 21:01:42 +0100

    Scrapbook

--- pages/scrapbook/Scrapbookfile.js
+++ pages/scrapbook/Scrapbookfile.js
@@ -8,15 +8,15 @@ function Scrapbookfile({ ...props }) {
   return (
     <section className="shadow-lg rounded-lg">
       <div className="p-5 bg-[#F8FBFF] dark:bg-[#2D2D2D] rounded-lg mb-5 shadow-md dark:text-white">
-        <div className="flex items-center gap-x-5 mb-5">
-          <Avatar image={userimg} className="relative  w-20 h-20" border="!border-[3px]" />
+        <div className="flex items-center gap-x-2 mb-5">
+          <Avatar image={userimg} className="relative  w-14 h-14" border="!border-[3px]" />
           <div className="flex-col">
             <p className="font-bold text-22px">@{username}</p>
             <h1 className="dark:text-white text-[#7D7D7D] -mt-1">{time}</h1>
           </div>
         </div>
 
-        <p className="mb-10 dark:text-white break-words">{text}</p>
+        <p className="mb-10 dark:text-white break-words text-16px">{text}</p>
         <div>
           {/* {image.map((img, index, image) => {
                 return (
