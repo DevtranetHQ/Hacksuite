5aaf5b9dd6222374fbc90ee432f2cd207eba899c
Mohammed Talha <sulemanshah432@gmail.com>
2022-05-29 18:25:59 +0530

    landing page - figma comments

--- pages/landing.js
+++ pages/landing.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import { Icon } from "@iconify/react";
-import Logo from "../components/Logo";
+import logoLight from "../public/assets/logo.svg";
 import Discord from "../components/icons/Discord";
 import landing from "../public/assets/landing/landing.svg";
 
@@ -18,74 +18,78 @@ import stellar from "../public/assets/landing/stellar.svg";
 export default function Landing({}) {
   return (
     <div className="">
-      <div className="flex items-center justify-between mxs:px-1 mxs:pt-2 px-8 pt-5">
-        <Logo className="mxs:w-[80px] w-[120px]" />
-        <div className="flex items-center mxs:text-10px text-24px">
-          <div className="flex items-center mxs:space-x-2 mxs:mx-2 space-x-16 mx-16">
+      <div className="flex items-center justify-between mxs:px-1 mxs:pt-2 px-8">
+        <div className="relative mxs:w-[80px] w-[120px] mt-6">
+          <Image src={logoLight} />
+        </div>
+        <div className="flex items-center mxs:text-10px text-20px xl:text-24px">
+          <div className="flex items-center mxs:space-x-2 mxs:mx-2 space-x-12 xl:space-x-16 mx-12 xl:mx-16">
             <Link href="/scrapbook">
-              <p className="mxs:text-10px text-[#7D7D7D]  cursor-pointer hover:opacity-80">
+              <p className="mxs:text-10px text-[#7D7D7D] text-20px xl:text-24px cursor-pointer hover:opacity-80">
                 Scrapbook
               </p>
             </Link>
             <Link href="/project-gallery">
-              <p className="mxs:text-10px text-[#7D7D7D]  cursor-pointer hover:opacity-80">
+              <p className="mxs:text-10px text-[#7D7D7D] text-20px xl:text-24px cursor-pointer hover:opacity-80">
                 Projects
               </p>
             </Link>
             <Link href="#">
-              <p className="mxs:text-10px text-[#7D7D7D]  cursor-pointer hover:opacity-80">
+              <p className="mxs:text-10px text-[#7D7D7D] text-20px xl:text-24px cursor-pointer hover:opacity-80">
                 People
               </p>
             </Link>
             <Link href="/events">
-              <p className="mxs:text-10px text-[#7D7D7D]  cursor-pointer hover:opacity-80">
+              <p className="mxs:text-10px text-[#7D7D7D] text-20px xl:text-24px cursor-pointer hover:opacity-80">
                 Events
               </p>
             </Link>
             <Link href="#">
-              <p className="mxs:text-10px text-[#7D7D7D]  cursor-pointer hover:opacity-80">Jobs</p>
+              <p className="mxs:text-10px text-[#7D7D7D] text-20px xl:text-24px cursor-pointer hover:opacity-80">
+                Jobs
+              </p>
             </Link>
           </div>
           <div className="flex items-center mxs:space-x-1 space-x-5">
             <Link href="#">
-              <button className="mxs:px-1 mxs:py-0.5 mxs:rounded-sm px-5 py-3 rounded-[10px] border border-[#4CB050] text-[#4CB050] bg-white font-bold transition-all hover:scale-105 focus:scale-105">
+              <button className="mxs:px-1 mxs:py-0.5 mxs:rounded-sm px-5 2xl:px-6 py-2 2xl:py-3.5 rounded-md border-[1.5px] border-[#4CB050] text-[#4CB050] bg-white font-bold transition-all hover:scale-105 focus:scale-105">
                 Hire Talents
               </button>
             </Link>
             <Link href="#">
-              <button className="mxs:px-1 mxs:py-0.5 mxs:rounded-sm px-5 py-3 rounded-[10px] bg-[#03A9F4] text-white font-bold transition-all hover:scale-105 focus:scale-105">
-                Sponser Events
+              <button className="mxs:px-1 mxs:py-0.5 mxs:rounded-sm px-5 2xl:px-6 py-2 2xl:py-3.5 rounded-md bg-[#03A9F4] text-white font-bold transition-all hover:scale-105 focus:scale-105">
+                Sponsor Events
               </button>
             </Link>
           </div>
         </div>
       </div>
 
-      <div className="mxs:mt-2 mxs:mx-1 mt-12 mx-5 flex grid grid-cols-2">
-        <div className="mxs:ml-1 mxs:pt-2 ml-7 flex flex-col pt-12">
-          <p className="font-bold leading-[1.3] mxs:text-16px text-48px">
+      <div className="mxs:mt-2 mxs:mx-1 mt-4 mx-5 flex grid grid-cols-2">
+        <div className="mxs:ml-1 mxs:pt-2 ml-3 flex flex-col pt-14 2xl:pt-[4.5rem]">
+          <p className="font-bold leading-[1.3] mxs:text-16px text-[40px] xl:text-48px">
             Where <span className="text-[#ff9700]">makers</span>,{" "}
             <span className="text-[#03a9f4]">developers</span>, and{" "}
             <span className="text-[#4cb050]">technologists</span> call home.
           </p>
-          <p className="mxs:text-12px mxs:mt-1 text-24px mt-7">
+          <p className="mxs:text-12px mxs:mt-1 text-20px xl:text-24px mt-5 2xl:mt-7">
             The Dynamics is a global network of makers, developers, and technologists where
             enthusiastic minds connect, build, share, and launch.
           </p>
-          <div className="flex items-center mxs:space-x-1 mxs:mt-1 space-x-5 mxs:text-10px text-24px mt-8">
+          <div className="flex items-center mxs:space-x-1 mxs:mt-1 space-x-5 mxs:text-10px text-20px xl:text-24px mt-6 2xl:mt-8">
             <Link href="/signup">
-              <button className="mxs:px-1 mxs:py-0.5 mxs:rounded-sm px-5 py-3 rounded-[10px] bg-[#03A9F4] text-white font-bold transition-all hover:scale-105 focus:scale-105">
+              <button className="mxs:px-1 mxs:py-0.5 mxs:rounded-sm px-5 2xl:px-6 py-2 2xl:py-3.5 rounded-md bg-[#03A9F4] text-white font-bold transition-all hover:scale-105 focus:scale-105">
                 Become a Member
               </button>
             </Link>
             <Link href="#">
-              <button className="mxs:px-1 mxs:py-0.5 mxs:rounded-sm flex items-center px-5 py-3 rounded-[10px] border border-[#03A9F4] text-[#03A9F4] bg-white font-bold transition-all hover:scale-105 focus:scale-105">
+              <button className="mxs:px-1 mxs:py-0.5 mxs:rounded-sm flex items-center px-5 2xl:px-6 py-2 2xl:py-3.5 rounded-md border-[1.5px] border-[#03A9F4] text-[#03A9F4] bg-white font-bold transition-all hover:scale-105 focus:scale-105">
                 Join our Discord&nbsp;&nbsp;
                 <Discord fill={"#03A9F4"} />
               </button>
             </Link>
           </div>
-          <p className="mxs:mt-1 flex items-center font-semibold mxs:text-[8px] text-20px text-[#7D7D7D] mt-5">
+          <p className="mxs:mt-1 flex items-center font-semibold mxs:text-[8px] text-[17px] xl:text-20px text-[#7D7D7D] mt-3.5 2xl:mt-5">
             Already a member?&nbsp;
             <Link href="/login">
               <span className="text-[#3B4FE4] flex items-center cursor-pointer">
@@ -110,9 +114,12 @@ export default function Landing({}) {
           <Image src={landing} alt="landing" />
         </div>
       </div>
-      <div className="mxs:mt-1 mt-8 mb-4 flex flex-col">
-        <p className="text-center mxs:text-[8px] text-18px font-bold text-[#A5A5A5]">WORKED WITH</p>
-        <div className="mxs:mx-[10px] mx-[140px] flex items-center justify-between">
+
+      <div className="mxs:mt-1 mt-6 2xl:mt-8 mb-3 2xl:mb-4 flex flex-col">
+        <p className="text-center mxs:text-[8px] text-[15px] xl:text-18px font-bold text-[#A5A5A5]">
+          WORKED WITH
+        </p>
+        <div className="mxs:mx-[10px] mx-[105px] xl:mx-[140px] flex items-center justify-between">
           <Image src={aws} alt="aws" />
           <Image src={digitalocean} alt="digitalocean" />
           <Image src={jetbrains} alt="jetbrains" />
