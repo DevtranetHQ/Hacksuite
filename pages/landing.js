a92dc0798e8abfe21f59e3deabdaf71eca50240e
Mohammed Talha <sulemanshah432@gmail.com>
2022-05-31 07:12:56 +0530

    fix height for view port xl

--- pages/landing.js
+++ pages/landing.js
@@ -65,18 +65,18 @@ export default function Landing({}) {
         </div>
       </div>
 
-      <div className="mxs:mt-2 mxs:mx-1 mt-2.5 xl:mt-4 mx-5 flex grid grid-cols-2">
-        <div className="mxs:ml-1 mxs:pt-2 ml-3 flex flex-col pt-10 lgm:pt-[4.5rem] xl:pt-14 2xl:pt-[4.5rem]">
-          <p className="font-bold leading-[1.3] mxs:text-16px text-[36px] xl:text-48px">
+      <div className="mxs:mt-2 mxs:mx-1 mt-2.5 xl:mt-3.5 mx-5 flex grid grid-cols-2">
+        <div className="mxs:ml-1 mxs:pt-2 ml-3 flex flex-col pt-10 lgm:pt-[4.5rem] xl:pt-16 2xl:pt-[4.5rem]">
+          <p className="font-bold leading-[1.3] mxs:text-16px text-[36px] xl:text-[44px] 2xl:text-48px">
             Where <span className="text-[#ff9700]">makers</span>,{" "}
             <span className="text-[#03a9f4]">developers</span>, and{" "}
             <span className="text-[#4cb050]">technologists</span> call home.
           </p>
-          <p className="mxs:text-12px mxs:mt-1 text-18px xl:text-24px mt-3.5 xl:mt-5 2xl:mt-7">
+          <p className="mxs:text-12px mxs:mt-1 text-18px xl:text-22px 2xl:text-24px mt-3.5 xl:mt-[1.125rem] 2xl:mt-7">
             The Dynamics is a global network of makers, developers, and technologists where
             enthusiastic minds connect, build, share, and launch.
           </p>
-          <div className="flex items-center mxs:space-x-1 mxs:mt-1 space-x-5 mxs:text-10px text-18px xl:text-24px mt-[1.125rem] xl:mt-6 2xl:mt-8">
+          <div className="flex items-center mxs:space-x-1 mxs:mt-1 space-x-5 mxs:text-10px text-18px xl:text-22px 2xl:text-24px mt-[1.125rem] xl:mt-[1.375rem] 2xl:mt-8">
             <Link href="/signup">
               <button className="mxs:px-1 mxs:py-0.5 mxs:rounded-sm px-5 2xl:px-6 py-2.5 2xl:py-3.5 rounded-md bg-[#03A9F4] text-white font-bold transition-all hover:scale-105 focus:scale-105">
                 Become a Member
@@ -89,7 +89,7 @@ export default function Landing({}) {
               </button>
             </Link>
           </div>
-          <p className="mxs:mt-1 flex items-center font-semibold mxs:text-[8px] text-[15.5px] xl:text-20px text-[#7D7D7D] mt-[0.5rem] xl:mt-3.5 2xl:mt-5">
+          <p className="mxs:mt-1 flex items-center font-semibold mxs:text-[8px] text-[15.5px] xl:text-18px 2xl:text-20px text-[#7D7D7D] mt-[0.5rem] xl:mt-3 2xl:mt-5">
             Already a member?&nbsp;
             <Link href="/login">
               <span className="text-[#3B4FE4] flex items-center cursor-pointer">
@@ -115,11 +115,11 @@ export default function Landing({}) {
         </div>
       </div>
 
-      <div className="mxs:mt-1 mt-3.5 lgm:mt-8 xl:mt-6 2xl:mt-8 mb-0 2xl:mb-4 flex flex-col">
-        <p className="text-center mxs:text-[8px] text-[14px] xl:text-[18px] font-bold text-[#A5A5A5]">
+      <div className="mxs:mt-1 mt-3.5 lgm:mt-8 xl:mt-[1.375rem] 2xl:mt-8 mb-0 2xl:mb-4 flex flex-col">
+        <p className="text-center mxs:text-[8px] text-[14px] xl:text-[16px] 2xl:text-18px font-bold text-[#A5A5A5]">
           WORKED WITH
         </p>
-        <div className="mxl:-mt-2 mxs:mx-[10px] mx-[60px] xl:mx-[140px] flex items-center justify-between">
+        <div className="mxl:-mt-2 m2xl:-mt-1 mxs:mx-[10px] mx-[60px] xl:mx-[140px] flex items-center justify-between">
           <Image src={aws} alt="aws" />
           <Image src={digitalocean} alt="digitalocean" />
           <Image src={jetbrains} alt="jetbrains" />
