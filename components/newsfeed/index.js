376c6f5bc98abe418c872360cc7e57ff11971598
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-01 16:47:56 +0100

    Chaged the cursor type on hover ohfhome icon

--- components/newsfeed/index.js
+++ components/newsfeed/index.js
@@ -38,9 +38,8 @@ export const TopNav = () => (
           darkClassName="mx-0 w-[25px] md:w-[33px] lg:w-[40px] h-[25px] md:h-[48px] lg:h-[60px]"
         />
         <Link
-          href="/"
-          className="scale-[0.53] lg:scale-[1] md:scale-[0.82]">
-          <HomeIcon />
+          href="/">
+          <HomeIcon className="scale-[0.53] lg:scale-[1] md:scale-[0.82] cursor-pointer"/>
         </Link>
       </div>
     </nav>
