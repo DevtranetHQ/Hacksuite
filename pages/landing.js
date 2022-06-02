c264c758f81765fc48332f2268891e4a36fd8e0b
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-02 20:52:43 +0100

    Added the LookingForSelectBoxes (with options) to the langind page

--- pages/landing.js
+++ pages/landing.js
@@ -17,6 +17,8 @@ import peopleImage from "../public/assets/TEST/people.png";
 import Scrapbookfile from "./scrapbook/Scrapbookfile";
 import ProjectGalleryProjectCard from "../components/project/ProjectGalleryProjectCard";
 import EventCard from "../components/event/EventCard";
+import LookingForSelectBoxes from "../components/LookingForSelectBoxes";
+
 
 export default function Landing({ user, projects, events, loggedIn }) {
   return (
@@ -214,43 +216,7 @@ export default function Landing({ user, projects, events, loggedIn }) {
         <div className="mt-9 py-20 rounded-bl-[20px] rounded-br-[20px] bg-[#F8FBFF] shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
           <p className="text-[90px] text-[#03A9F4] text-center mb-12">Who are you looking for?</p>
           <div className="flex justify-center items-end">
-            <div>
-              <p className="text-14px lg:text-18px xl:text-22px 2xl:text-24px pl-4 mb-3 font-semibold">
-                Searching for
-              </p>
-              <div className="flex">
-                <input
-                  className="rounded-bl-[10px] rounded-tl-[10px] border-[3px] border-[#03A9F4] py-6 px-6"
-                  placeholder="Developers, Angel Investors"
-                />
-                <div className="bg-[#FF9700] w-3 py-6"></div>
-              </div>
-            </div>
-
-            <div>
-              <p className="text-14px lg:text-18px xl:text-22px 2xl:text-24px pl-4 mb-3 font-semibold">
-                Available for
-              </p>
-              <div className="flex">
-                <input
-                  className="border-[3px] border-[#03A9F4] py-6 px-6"
-                  placeholder="Full-time Job, Investing"
-                />
-                <div className="p-[1.390rem] bg-[#03A9F4] rounded-br-[10px] rounded-tr-[10px]">
-                  <svg
-                    width="35"
-                    height="33"
-                    viewBox="0 0 35 33"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      d="M22.832 14.0001C26.9987 14.0001 30.332 17.3334 30.332 21.5001C30.332 22.9667 29.9154 24.3501 29.182 25.5001L34.3154 30.6667L31.9987 32.9834L26.7987 27.8667C25.6487 28.5834 24.282 29.0001 22.832 29.0001C18.6654 29.0001 15.332 25.6667 15.332 21.5001C15.332 17.3334 18.6654 14.0001 22.832 14.0001ZM22.832 17.3334C21.727 17.3334 20.6672 17.7724 19.8858 18.5538C19.1044 19.3352 18.6654 20.395 18.6654 21.5001C18.6654 22.6051 19.1044 23.665 19.8858 24.4464C20.6672 25.2278 21.727 25.6667 22.832 25.6667C23.9371 25.6667 24.9969 25.2278 25.7783 24.4464C26.5597 23.665 26.9987 22.6051 26.9987 21.5001C26.9987 20.395 26.5597 19.3352 25.7783 18.5538C24.9969 17.7724 23.9371 17.3334 22.832 17.3334ZM13.6654 0.666748C15.4335 0.666748 17.1292 1.36913 18.3794 2.61937C19.6297 3.86961 20.332 5.5653 20.332 7.33341C20.332 8.85008 19.8154 10.2501 18.9654 11.3834C17.532 11.9167 16.2487 12.7667 15.182 13.8334L13.6654 14.0001C11.8973 14.0001 10.2016 13.2977 8.95132 12.0475C7.70108 10.7972 6.9987 9.10152 6.9987 7.33341C6.9987 5.5653 7.70108 3.86961 8.95132 2.61937C10.2016 1.36913 11.8973 0.666748 13.6654 0.666748V0.666748ZM0.332031 27.3334V24.0001C0.332031 20.4667 5.8487 17.5667 12.832 17.3334C12.2987 18.6334 11.9987 20.0334 11.9987 21.5001C11.9987 23.6501 12.632 25.6667 13.6654 27.3334H0.332031Z"
-                      fill="white"
-                    />
-                  </svg>
-                </div>
-              </div>
-            </div>
+            <LookingForSelectBoxes />
           </div>
         </div>
       </div>
