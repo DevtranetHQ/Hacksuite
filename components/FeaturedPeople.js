fa69482b3a03b7ce5c5888a04704bf498abe84c9
Zobam <zobamokoli7@gmail.com>
2022-06-06 15:10:17 +0100

    HomePge Responsivness

--- components/FeaturedPeople.js
+++ components/FeaturedPeople.js
@@ -1,73 +1,30 @@
 import React from "react";
 import Avatar from "./Avatar";
-import ProfileImg from "../public/assets/TEST/profile.jpg";
 import ArrowIcon from "./icons/Arrow";
+import VerifiedIcon from "./icons/VerifiedIcon";
 
-export default function FeaturedPeople() {
+export default function FeaturedPeople({ ...props }) {
+  const { headline, following, userimg, username, verified, people } = props;
   return (
     <div>
-      <h1 className="font-semibold text-20px text-left mb-2">Featured People</h1>
       <div className="bg-[#F8FBFF] h-screen rounded-md dark:bg-[#2D2D2D]">
-        <div className="flex items-center justify-between p-4">
+        <div className="flex items-center justify-between p-4  border-b">
           <div className="flex gap-2">
-            <Avatar image={ProfileImg} className="relative h-10 w-10" />
+            <Avatar image={userimg} className="relative h-10 w-10" />
             <div className="flex flex-col justify-start">
-              <span className="text-left font-bold text-16px">Bella See</span>
-              <span className="-mt-1 text-14px">Founder, CommandTech</span>
+              <span className="text-left font-bold text-16px flex items-center">
+                {username} {verified && <VerifiedIcon className="h-5 w-5 ml-2" />}
+              </span>
+              <span className="-mt-1 text-14px">{headline}</span>
             </div>
           </div>
-          <p className="bg-deep-sky-blue px-2 py-1 rounded-md text-white text-14px ">Unfollow</p>
-        </div>
-        <hr />
-        <div className="flex items-center justify-between p-4">
-          <div className="flex gap-2">
-            <Avatar image={ProfileImg} className="relative h-10 w-10" />
-            <div className="flex flex-col justify-start ">
-              <span className="text-left font-bold text-16px">Ibrahim Salami</span>
-              <span className="-mt-1 text-14px">Founder, CommandTech</span>
-            </div>
-          </div>
-          <p className="bg-white dark:bg-transparent border border-[#C9C9C9] px-[13px] py-1 rounded-md  text-14px ">
-            Follow
-          </p>
-        </div>
-        <hr />
-        <div className="flex items-center justify-between p-4">
-          <div className="flex gap-2">
-            <Avatar image={ProfileImg} className="relative h-10 w-10" />
-            <div className="flex flex-col justify-start ">
-              <span className="text-left font-bold text-16px">Dora Palfi</span>
-              <span className="-mt-1 text-14px">Founder, CommandTech</span>
-            </div>
-          </div>
-          <p className="bg-white dark:bg-transparent border border-[#C9C9C9] px-[13px] py-1 rounded-md  text-14px ">
-            Follow
-          </p>
-        </div>
-        <hr />
-        <div className="flex items-center justify-between p-4">
-          <div className="flex gap-2">
-            <Avatar image={ProfileImg} className="relative h-10 w-10" />
-            <div className="flex flex-col justify-start ">
-              <span className="text-left font-bold text-16px">Dev Agrawal</span>
-              <span className="-mt-1 text-14px">Founder, CommandTech</span>
-            </div>
-          </div>
-          <p className="bg-white dark:bg-transparent border border-[#C9C9C9] px-[13px] py-1 rounded-md  text-14px ">
-            Follow
-          </p>
-        </div>
-        <hr />
-        <div className="flex items-center justify-between p-4">
-          <div className="flex gap-2">
-            <Avatar image={ProfileImg} className="relative h-10 w-10" />
-            <div className="flex flex-col justify-start ">
-              <span className="text-left font-bold text-16px">Melinda Gates</span>
-              <span className="-mt-1 text-14px">Founder, CommandTech</span>
-            </div>
-          </div>
-          <p className="bg-white dark:bg-transparent border border-[#C9C9C9] px-[13px] py-1 rounded-md  text-14px ">
-            Follow
+          <p
+            className={`px-2 py-1 rounded-md ${
+              following
+                ? "bg-deep-sky-blue text-white"
+                : "bg-[#FFFFFF] text-black border border-[#C9C9C9]"
+            } text-14px`}>
+            {following ? "Unfollow" : "Follow"}
           </p>
         </div>
         <button className="button-deep-sky-blue button-small mx-auto mt-8 items-center ">
