00191cc76dfcc2c680f53e6cd7e444fcdaf79b51
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-07 13:10:22 +0100

    Mapped peopleData to the peopleSearchPerso component

--- pages/newsfeed/people_search.tsx
+++ pages/newsfeed/people_search.tsx
@@ -5,6 +5,7 @@ import MessageForm from "../../components/MessageForm";
 import { PeopleData } from "../../components/people/PeopleInfo";
 import PersonCard from "../../components/people/PersonCard";
 import LookingForSelect from "../../components/people/LookingFor";
+import PeopleSearchPerson from "../../components/people/PeopleSearchPerson";
 
 const PeopleSearch : React.FC = () => {
     return (
@@ -17,6 +18,32 @@ const PeopleSearch : React.FC = () => {
                 </div>
                 <div>
                     <p className="text-[48px] font-semibold text-center mt-[92px] mb-[31px]">Search result for "Zach Latta"</p>
+                    <p className="text-[36px] text-[#A5A5A5] font-normal text-center mb-[30px]">1 result</p>
+                    <PeopleSearchPerson verified={true} name="Belle See" followed={false} roles="Senior Software engineer" hasBottomBorder={true}/>
+                    <div className="pt-[111px]">
+                        <p className="text-[48px] font-semibold text-center mb-[40px]">Featured people</p>
+                        {
+                            PeopleData.map(person => (
+                                <PeopleSearchPerson 
+                                key={person.id} 
+                                name={person.name} 
+                                verified={person.verified} 
+                                roles={person.roles} 
+                                followed={person.followed} 
+                                hasBottomBorder={PeopleData.indexOf(person) + 1 == PeopleData.length}
+                                />
+                            ))
+                        }
+                        <div className="w-full flex justify-center items-center mt-[70px] lg:mt-[80px] mb-[45px] lg:mb-[64px]">
+                            <button
+                                className="text-white text-[16px] lg:text-[48px] py-[13px] lg:py-[48px] px-[17px] lg:px-[44px] font-bold rounded-lg lg:rounded-[15px] bg-[#03a9f4] lg:button-big button-deep-sky-blue inline-flex items-center gap-x-3 lg:gap-x-[48px]">
+                                <span>View more</span>
+                                <div className="lg:scale-[2] relative lg:top-1">
+                                <ArrowIcon />
+                                </div>
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </section>
         </>
