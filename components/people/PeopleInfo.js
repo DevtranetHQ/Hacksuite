a3d20ea79bb3ee8f19612090e020affdb2807724
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-07 12:54:43 +0100

    Added the followed prop to peopleInfo data

--- components/people/PeopleInfo.js
+++ components/people/PeopleInfo.js
@@ -4,76 +4,87 @@ export const PeopleData = [
     image: "/assets/TEST/people.png",
     name: "Belle See",
     verified: true,
-    roles: "Founder, CommandTech"
+    roles: "Founder, CommandTech", 
+    followed: false
   },
   {
     id: 2,
     image: "/assets/TEST/people.png",
     name: "Agnes M. Reilly",
     verified: false,
-    roles: "Founder, CommandTech"
+    roles: "Founder, CommandTech", 
+    followed: true
   },
   {
     id: 3,
     image: "/assets/TEST/people.png",
     name: "Timothy D. Jones",
     verified: false,
-    roles: "Founder, CommandTech"
+    roles: "Founder, CommandTech", 
+    followed: false
   },
   {
     id: 4,
     image: "/assets/TEST/people.png",
     name: "Megan A. Lear",
     verified: false,
-    roles: "Founder, CommandTech"
+    roles: "Founder, CommandTech", 
+    followed: true
   },
   {
     id: 5,
     image: "/assets/TEST/people.png",
     name: "Lillian T. Bell",
     verified: true,
-    roles: "Founder, CommandTech"
+    roles: "Founder, CommandTech", 
+    followed: false
   },
   {
     id: 6,
     image: "/assets/TEST/people.png",
     name: "Constance A. Klemm",
     verified: true,
-    roles: "Founder, CommandTech"
+    roles: "Founder, CommandTech", 
+    followed: false
   },
   {
     id: 7,
     image: "/assets/TEST/people.png",
     name: "Karen J. Waugh",
     verified: true,
-    roles: "Founder, CommandTech"
+    roles: "Founder, CommandTech", 
+    followed: false
   },
   {
     id: 8,
     image: "/assets/TEST/people.png",
     name: "Lucas A. Garrison",
     verified: true,
-    roles: "Founder, CommandTech"
+    roles: "Founder, CommandTech", 
+    followed: false
   },
   {
     id: 9,
     image: "/assets/TEST/people.png",
     name: "Harry B. Carpenter",
     verified: true,
-    roles: "Founder, CommandTech"
+    roles: "Founder, CommandTech", 
+    followed: false
   },
   {
     id: 10,
     image: "/assets/TEST/people.png",
     name: "Harmony J. Ware",
     verified: false,
-    roles: "Founder, CommandTech"
+    roles: "Founder, CommandTech", 
+    followed: false
   },
   {
     id: 11,
     image: "/assets/TEST/people.png",
     name: "Ruth D. Geter",
     verified: false,
-    roles: "Founder, CommandTech"
+    roles: "Founder, CommandTech", 
+    followed: false
   }
 ];
