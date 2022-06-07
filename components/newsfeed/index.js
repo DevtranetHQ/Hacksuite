cac896bf18bd9d6a7f1075e26872693a953ecef7
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-07 18:54:20 +0100

    Added search functionality for the people page to the FullNav

--- components/newsfeed/index.js
+++ components/newsfeed/index.js
@@ -3,14 +3,22 @@ import Logo from "../../components/Logo";
 import DarkModeToggle from "../../components/DarkModeToggle";
 import Link from "next/link";
 import HomeIcon from "../icons/HomeIcon";
+import { useState } from "react";
 
-export const TopNav = () => (
+export const TopNav = ({setSearchValue, setSearchPageIsShowing}) => {
+  const [input, setInput] = useState("");
+
+  return (
   <div className="dark:text-white">
     <nav className="flex items-center justify-between pl-[10px] lg:pl-8 pr-[12px] lg:pr-12">
       <Logo className="w-[80px] md:w-[120px] py-5" />
       <form className="bg-transparent flex items-center ml-auto m-0 justify-center mt-1 pr-2 dark:bg-transparent text-white dark:bg-white">
         <div className="relative rounded-md flex  items-center p-1 border-[#03A9F4] border-[3px]">
-          <div className="absolute pl-3 z-10 top-0 inset-y-0  flex items-center pointer-events-none">
+          <div onClick={setSearchValue && setSearchPageIsShowing ? () => {
+            setSearchValue(input);
+            setSearchPageIsShowing(true)
+          } : () => null}
+          className="absolute pl-3 z-10 top-0 inset-y-0  flex items-center cursor-pointer">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5 text-[#A5A5A5] flex items-center dark:text-white"
@@ -29,6 +37,8 @@ export const TopNav = () => (
             type="text"
             placeholder="I’m looking for..."
             className="dark:placeholder:text-white block w-full pl-10  dark:bg-transparent rounded-lg form-input border-none p-0 m-0 py-2 dark:text-white text-black"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
           />
         </div>
       </form>
@@ -45,61 +55,66 @@ export const TopNav = () => (
       </div>
     </nav>
   </div>
-);
 
-const FullNav = () => {
+  );
+}
+
+
+const FullNav = ({setSearchValue, setSearchPageIsShowing}) => {
   const { pathname } = useRouter();
   return (
-    <div className="dark:bg-[#202020] dark:text-white pb-10">
-      <TopNav />
-      <nav className="w-[max-content] mx-auto mt-3">
-        <ul className="flex gap-x-[3rem] w-[max-content] text-20px">
-          <Link href="/newsfeed">
-            <li
-              className={`${
-                pathname === "/newsfeed"
-                  ? "font-bold border-b-[3px] border-b-orange-peel"
-                  : "font-bold text-[#7D7D7D]"
-              } cursor-pointer`}>
-              SCRAPBOOK
-            </li>
-          </Link>
 
-          <Link href="/newsfeed/projects">
-            <li
-              className={`${
-                pathname === "/newsfeed/projects"
-                  ? "font-bold border-b-[3px] border-b-orange-peel"
-                  : "font-bold text-[#7D7D7D]"
-              } cursor-pointer`}>
-              PROJECTS
-            </li>
-          </Link>
+      <div className="dark:bg-[#202020] dark:text-white pb-10">
+        <TopNav setSearchValue={setSearchValue} setSearchPageIsShowing={setSearchPageIsShowing}/>
+        <nav className="w-[max-content] mx-auto mt-3">
+          <ul className="flex gap-x-[3rem] w-[max-content] text-20px">
+            <Link href="/newsfeed">
+              <li
+                className={`${
+                  pathname === "/newsfeed"
+                    ? "font-bold border-b-[3px] border-b-orange-peel"
+                    : "font-bold text-[#7D7D7D]"
+                } cursor-pointer`}>
+                SCRAPBOOK
+              </li>
+            </Link>
 
-          <Link href="/newsfeed/people">
-            <li
-              className={`${
-                pathname === "/newsfeed/people"
-                  ? "font-bold border-b-[3px] border-b-orange-peel"
-                  : "font-bold text-[#7D7D7D]"
-              } cursor-pointer`}>
-              PEOPLE
-            </li>
-          </Link>
+            <Link href="/newsfeed/projects">
+              <li
+                className={`${
+                  pathname === "/newsfeed/projects"
+                    ? "font-bold border-b-[3px] border-b-orange-peel"
+                    : "font-bold text-[#7D7D7D]"
+                } cursor-pointer`}>
+                PROJECTS
+              </li>
+            </Link>
 
-          <Link href="/newsfeed/jobs">
-            <li
-              className={`${
-                pathname === "/newsfeed/jobs"
-                  ? "font-bold border-b-[3px] border-b-orange-peel"
-                  : "font-bold text-[#7D7D7D]"
-              } cursor-pointer`}>
-              JOBS
-            </li>
-          </Link>
-        </ul>
-      </nav>
-    </div>
+            <Link href="/newsfeed/people">
+              <li
+                className={`${
+                  pathname === "/newsfeed/people"
+                    ? "font-bold border-b-[3px] border-b-orange-peel"
+                    : "font-bold text-[#7D7D7D]"
+                } cursor-pointer`}>
+                PEOPLE
+              </li>
+            </Link>
+
+            <Link href="/newsfeed/jobs">
+              <li
+                className={`${
+                  pathname === "/newsfeed/jobs"
+                    ? "font-bold border-b-[3px] border-b-orange-peel"
+                    : "font-bold text-[#7D7D7D]"
+                } cursor-pointer`}>
+                JOBS
+              </li>
+            </Link>
+          </ul>
+        </nav>
+      </div>
+
   );
 };
 
