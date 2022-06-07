a4b4f2aa8a06dcf8280072af476ba9ebdb75adfe
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-07 12:18:42 +0100

    Worked on functionality for follow and unfollow in the PeopleSearchPerson component

--- components/people/PeopleSearchPerson.tsx
+++ components/people/PeopleSearchPerson.tsx
@@ -1,4 +1,7 @@
-const PeopleSearchPerson = () => {
+import { useState } from "react";
+
+const PeopleSearchPerson : React.FC = () => {
+    const [isFollowing, setIsFollowing] = useState(false);
     return (
         <div className="border-t border-b border-[#C9C9C9] py-[28px]">
             <div className="max-w-[950px] mx-auto">
@@ -12,9 +15,16 @@ const PeopleSearchPerson = () => {
                             <p className="text-[#2D2D2D] text-[20px]">Software Engineer, Google</p>
                         </div>
                     </div>
-                    <button className="h-[52px] px-[30px] text-[24px] border border-[#C9C9C9] rounded-[10px]">
+                    {!isFollowing && 
+                    <button onClick={() => setIsFollowing(!isFollowing)} className="h-[52px] px-[30px] text-[24px] border border-[#C9C9C9] rounded-[10px]">
                         Follow
+                    </button>}
+                    {
+                        isFollowing &&
+                    <button onClick={() => setIsFollowing(!isFollowing)} className="relative left-[24px] h-[52px] bg-[#03A9F4] text-white px-[30px] text-[24px] border-[#C9C9C9] rounded-[10px]">
+                        Unfollow
                     </button>
+                    }
                 </div>
             </div>
         </div>
