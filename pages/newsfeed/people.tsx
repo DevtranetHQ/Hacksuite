38b1a2d5e4b268c9026366ab4cb0400d024f8924
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-08 12:21:32 +0100

    Formatted all the files in pages/newsfeed/

--- pages/newsfeed/people.tsx
+++ pages/newsfeed/people.tsx
@@ -9,30 +9,21 @@ import PeopleSearch from "../../components/people/PeopleSearch";
 const People = ({ people }) => {
   const [showMessage, setShowMessage] = useState(false);
   const [searchPageIsShowing, setSearchPageIsShowing] = useState(false);
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState("");
 
   return (
-
     <>
-      <FullNav setSearchPageIsShowing={setSearchPageIsShowing} setSearchValue={setSearchValue}/>
+      <FullNav setSearchPageIsShowing={setSearchPageIsShowing} setSearchValue={setSearchValue} />
       {showMessage ? <MessageForm setShowMessage={setShowMessage} /> : null}
       <section className="dark:bg-black pb-[60px]">
         <div className="bg-[#F8FBFF] dark:bg-[#2D2D2D] w-[100%] text-center flex flex-col items-center justify-center pt-[71px] pb-[83px] rounded-b-[20px] shadow-[0px_4px_4px_rgba(0,0,0,0.25)]">
           <h2 className="text-[90px] text-deep-sky-blue font-bold">Who are you looking for?</h2>
           <LookingForSelect />
         </div>
-          {
-            searchPageIsShowing &&
-            <PeopleSearch people={people} searchValue={searchValue}/>
-          }
-          {
-            !searchPageIsShowing &&
-            <PeoplePage people={people} setShowMessage={setShowMessage}/>
-          }
-
+        {searchPageIsShowing && <PeopleSearch people={people} searchValue={searchValue} />}
+        {!searchPageIsShowing && <PeoplePage people={people} setShowMessage={setShowMessage} />}
       </section>
     </>
-
   );
 };
 
