b385a9ff8610ef789be49d7e491a378ea5e8e372
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-08 12:49:41 +0100

    Removed formatting from SelectStyles.js

--- components/people/SelectStyles.js
+++ components/people/SelectStyles.js
@@ -1,43 +1,43 @@
 export const searchingForStyles = {
-  control: styles => ({
-    ...styles,
-    "backgroundColor": "white",
-    "paddingTop": 3.5,
-    "width": "256px",
-    "paddingBottom": 3.5,
-    "border": "3px solid #03A9F4",
-    "borderRadius": 0,
-    "borderTopLeftRadius": 5,
-    "borderBottomLeftRadius": 5,
-    "marginTop": 2,
-    "&:hover": {
-      borderColor: "#03A9F4"
-    }
-  }),
-  option: (styles, { data, isDisabled, isFocused, isSelected }) => {
-    return {
-      ...styles
-    };
-  }
-};
-
-export const availableForStyles = {
-  control: styles => ({
-    ...styles,
-    "backgroundColor": "white",
-    "paddingTop": 3.5,
-    "width": "256px",
-    "paddingBottom": 3.5,
-    "border": "3px solid #03A9F4",
-    "borderRadius": 0,
-    "marginTop": 2,
-    "&:hover": {
-      borderColor: "#03A9F4"
-    }
-  }),
-  option: (styles, { data, isDisabled, isFocused, isSelected }) => {
-    return {
-      ...styles
-    };
-  }
-};
+    control: styles => ({ 
+      ...styles, 
+      backgroundColor: 'white', 
+      paddingTop: 3.5, 
+      width: '256px', 
+      paddingBottom: 3.5, 
+      border: '3px solid #03A9F4', 
+      borderRadius: 0, 
+      borderTopLeftRadius: 5, 
+      borderBottomLeftRadius: 5, 
+      marginTop: 2, 
+      "&:hover": {
+        borderColor: "#03A9F4"
+      }
+    }),
+    option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+      return {
+        ...styles
+      };
+    },
+  };
+  
+  export const availableForStyles = {
+    control: styles => ({ 
+      ...styles, backgroundColor: 'white', 
+      paddingTop: 3.5, 
+      width: '256px', 
+      paddingBottom: 3.5, 
+      border: '3px solid #03A9F4', 
+      borderRadius: 0, 
+      marginTop: 2, 
+      "&:hover": {
+        borderColor: "#03A9F4"
+      }
+    }),
+    option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+      return {
+        ...styles
+      };
+    },
+  };
+  
