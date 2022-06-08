c5a07a4eac266b402a039415264bb50061073d4d
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-06-08 15:49:30 +0100

    Made the header text responsice

--- pages/newsfeed/people.tsx
+++ pages/newsfeed/people.tsx
@@ -17,7 +17,7 @@ const People = ({ people }) => {
       {showMessage ? <MessageForm setShowMessage={setShowMessage} /> : null}
       <section className="dark:bg-black pb-[60px]">
         <div className="bg-[#F8FBFF] dark:bg-[#2D2D2D] w-[100%] text-center flex flex-col items-center justify-center pt-[71px] pb-[83px] rounded-b-[20px] shadow-[0px_4px_4px_rgba(0,0,0,0.25)]">
-          <h2 className="text-[90px] text-deep-sky-blue font-bold">Who are you looking for?</h2>
+          <h2 className="text-[28px] xs:text-[36px] sm:text-[52px] md:text-[64px] lg:text-[80px] xl:text-[90px] mb-[20px] md:mb-[25px] lg:mb-[40px] xl:mb-[50px] text-deep-sky-blue font-bold ">Who are you looking for?</h2>
           <LookingForSelect />
         </div>
         {searchPageIsShowing && <PeopleSearch people={people} searchValue={searchValue} />}
